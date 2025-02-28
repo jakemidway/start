e3f7467d835dcfa0a27602807a7ea8344c448afb
Yauheni Miadzvetski <jakemidway@gmail.com>
2025-02-27 21:27:16 -0500

    nichego

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,23 @@ import styled from "styled-components";
 function App() {
     return (
         <div className="App">
+
             <Title>Welcome to IT-INCUBATOR</Title>
+            <Header>
+
+            </Header>
+            )
+
         </div>
     )
 }
 
 export default App
+
+const Header = styled.header`
+    background-color: aquamarine;
+`
+
 
 const Title = styled.h1`
     font-size: 1.5em;
